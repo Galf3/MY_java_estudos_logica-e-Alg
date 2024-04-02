b611790b4012c3f5d2c43d9c4175d0725cd9f29c
Rafael Rodrigues <rafael.rodriguez098@gmail.com>
2024-04-02 15:15:16 -0300

    Operadores logicos -  exemplo com NOT e AND

---
+++ src/Exemplos_logicAlg/OperadoresLógicos.java
@@ -0,0 +1,38 @@
+package Exemplos_logicAlg;
+
+public class OperadoresLógicos {
+	
+	public static void main(String[] args) {
+		
+		boolean res;
+		
+		
+		  //////////// ***** NEGAÇÃO NOT  ***********//////////
+		
+		res = ! (( 4+ 3 ) != 17);  // negando toda expressão e atribuindo a variavel res
+		
+		// equivale a expressão NOT( 4 + 3) é diferente de 17
+		
+		System.out.println (res);
+		
+		System.out.println();
+		
+		 //////////// ***** CONJUNÇÃO AND  ***********//////////
+		
+		res = ! (( 4+ 3 ) != 17); 
+		System.out.println ("primeiro = " + res);
+		res = (4>3) && (4 != 2);
+		
+		System.out.println(" segunda = " + res);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
