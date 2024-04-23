d5c448cbd806eb082585a195558cc1dc81e7765a
Rafael Rodrigues <rafael.rodriguez098@gmail.com>
2024-04-23 10:45:16 -0300

    exemplo usando loop While.

---
+++ src/ExWhile/LaçoWhile.java
@@ -0,0 +1,20 @@
+package ExWhile;
+
+public class LaçoWhile {
+
+	public static void main(String[] args) {
+	int i =1;
+	
+	int max = 10;
+	
+	System.out.println("Contando até " + max);
+	
+	while ( i<=max) {
+		
+		System.out.println("Valor de i: " + i);
+		
+		i++;
+	}
+	System.out.println(i);
+}
+}
